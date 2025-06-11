57b579f798a70f183ef6c7bacd56a0736d552bbf
Kathy Xiong <kathy.kxiong16@gmail.com>
2025-06-11 15:06:11 -0500

    Created Transactions class in BL Models

--- BackEnd/NumoApp/NumoApp.BL.Models/obj/Debug/net8.0/NumoApp.BL.Models.AssemblyInfo.cs
+++ BackEnd/NumoApp/NumoApp.BL.Models/obj/Debug/net8.0/NumoApp.BL.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumoApp.BL.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1917a649eb3ffb6dcbd112880052b6e563fb2ad6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265395b9972ed35ba5b20e5ee534e910fe9e5236")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumoApp.BL.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumoApp.BL.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
