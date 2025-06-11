ca65457ae432d0d8c5944a1771e34a6e7af302d9
Kathy Xiong <kathy.kxiong16@gmail.com>
2025-06-11 15:11:08 -0500

    Created Crypto class in BL Models

--- BackEnd/NumoApp/NumoApp.BL.Models/obj/Debug/net8.0/NumoApp.BL.Models.AssemblyInfo.cs
+++ BackEnd/NumoApp/NumoApp.BL.Models/obj/Debug/net8.0/NumoApp.BL.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumoApp.BL.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b579f798a70f183ef6c7bacd56a0736d552bbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda22a62f2ea31f0d0781f6f417362ec3fc37216")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumoApp.BL.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumoApp.BL.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
